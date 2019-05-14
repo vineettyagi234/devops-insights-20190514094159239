1be7b37e154c5ed94fcd3a35eb6a5bb24704c6f5
vineettyagi234 <50137681+vineettyagi234@users.noreply.github.com>
2019-05-14 22:27:42 +1200

    Update apiv1.spec.js

--- tests/server/apiv1.spec.js
+++ tests/server/apiv1.spec.js
@@ -109,7 +109,7 @@
 
       assert(resMock.status.lastCall.calledWith(200), 'Unexpected response:' + resMock.status.lastCall.args);
       assert(resMock.send.lastCall.args[0].city === 'hamilton', 'Unexpected response:' + resMock.send.lastCall.args[0].city);
-      assert(resMock.send.lastCall.args[0].weather === 'Conditions are cold and temperature is 75 F', 'Unexpected response:' + resMock.send.lastCall.args[0].weather);
+      assert(resMock.send.lastCall.args[0].weather === 'Conditions are clear and temperature is 75 F', 'Unexpected response:' + resMock.send.lastCall.args[0].weather);
     });
   });
 
