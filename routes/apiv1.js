923e89fe17e56a26cfdaa236de4e8345ecc548fb
vineettyagi234 <50137681+vineettyagi234@users.noreply.github.com>
2019-05-16 12:06:05 +1200

    Update apiv1.js

--- routes/apiv1.js
+++ routes/apiv1.js
@@ -28,7 +28,7 @@ exports.getWeather = function(req, res) {
     	} else {
     		if(body.cod === 200) {
     			var weath = "Conditions are " + body.weather[0].main + " and temperature is " + body.main.temp + ' C';
-    			var response = {city: body.name, weather: weath, lat:coord.lat, lng: coord.};
+    			var response = {city: body.name, weather: weath, lat:coord.lat, lng: coord.lon};
     			return res.status(200).send(response);
     		} else {
                 return res.status(400).send({msg:'Failed'});
