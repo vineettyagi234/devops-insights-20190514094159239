ed3b2ac75d766d34fbd0dbae42d79858e2c8545b
vineettyagi234 <50137681+vineettyagi234@users.noreply.github.com>
2019-05-17 10:54:06 +1200

    Update controllers.js

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -22,15 +22,20 @@ function initMap() {
             var xhttp = new XMLHttpRequest();
 
             xhttp.onload = function(){
-                var ourData = JSON.parse(xhttp.response.data);
-                document.getElementById("demo").innerHTML = ourData.city;
-                document.getElementById("zip1Weather").innerHTML = ourData.weather;
+                var res = JSON.parse(this.responseText);
+                
+                var lat = res.coord.lat;
+                var lng = res.coord.lon;
+                var cityName = res.name;
+                var weather = res.weather.main;
+                document.getElementById("zip1City").innerHTML = res.name;
+                document.getElementById("zip1Weather").innerHTML = res.weather;
 
                 point(response.data.lat, response.data.lng);
 
             };
 
-            xhttp.open("GET", "/api/v1/getWeather?lat=' + tik.latLng.lat() +'&lon=' + tik.latLng.lon()", true);
+            xhttp.open("GET", "/api/v1/getWeather?lat=" + lat + "&lon=" + lng + "&name" + cityName, true);
             xhttp.send();
             });
 
